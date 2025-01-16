30b5306f993e6cf9ce4ba388605e3407f6c54632
Pactivisme <paul@delhomme.ovh>
2025-01-16 16:17:01 +0100

    Ajout des fonctionnalité de base et connexion srv

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const sequelize = require('./config/database');
+const userRoutes = require('./routes/users');
+const User = require('./models/User');
+
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Salut, monde !');
-});
+app.use(bodyParser.json());
 
+app.use('/api/utilisateurs', userRoutes);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connexion à la base de données réussie.');
+  })
+  .catch((err) => {
+    console.error('Impossible de se connecter à la base de données :', err);
+  });
+
+sequelize
+  .sync({ alter: true })
+  .then(() => {
+    console.log('Base de données synchronisée.');
+  })
+  .catch((err) => {
+    console.error("Erreur lors de la synchronisation de la base de données : ", err);
+  });
+
+// Lancement serveur
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Serveur en cours d'exécution à l'adresse http://localhost:${port}`);
+  console.log(`Serveur en cours d'éxecution sur http://localhost:${port}`);
 });
